35714a2c20e7ba8e6ec12ccc38a3109c07f55427
Marc Lopez <marc5.12@outlook.com>
2016-04-27 17:04:31 +0200

    Update scripts, to provide more robust functionality
    Signed-off-by: Marc Lopez <marc5.12@outlook.com>

--- 3.4/docker-entrypoint.sh
+++ 3.4/docker-entrypoint.sh
@@ -1,41 +1,50 @@
 #!/bin/bash
 
+PRE_DIR="${CODE_DIR}/.crafter/scripts/api/pre"
+OVERRIDE_DIR="${CODE_DIR}/.crafter/scripts/api/override"
+POST_DIR="${CODE_DIR}/.crafter/scripts/api/post"
+
 # Pre Reqs User Scripts
-SCRIPTS=$(find ./.crafter/pre -type f -name '*.sh')
+if [[ -d ${PRE_DIR} ]]; then
+  SCRIPTS=$(find ${PRE_DIR} -type f -name '*.sh')
 
-if [[ ! -z ${SCRIPTS} ]]; then
-  for script in ${SCRIPTS}; do
-    ${script}
-  done
-fi
-
-# Override Reqs user scripts
-SCRIPTS=$(find ./.crafter/override -type f -name '*.sh')
-
-if [[ ! -z ${SCRIPTS} ]]; then
-  for script in ${SCRIPTS}; do
-    ${script}
-  done
-else
-  # Find requirements
-  REQUIREMENTS=$(find . -name requirements.txt -not -path venv -not -path virtualenv)
-
-  if [[ ! -z ${REQUIREMENTS} ]]; then
-    for requirement in ${REQUIREMENTS}; do
-      pip install -r ${requirement}
+  if [[ ! -z ${SCRIPTS} ]]; then
+    for script in ${SCRIPTS}; do
+      ${script}
     done
   fi
 fi
 
-# Post Reqs User Scripts
-SCRIPTS=$(find ./.crafter/post -type f -name '*.sh')
+# Override Reqs user scripts
+if [[ -d ${OVERRIDE_DIR} ]]; then
+  SCRIPTS=$(find ${CODE_DIR}/.crafter/scripts/api/override -type f -name '*.sh')
 
-if [[ ! -z ${SCRIPTS} ]]; then
-  for script in ${SCRIPTS}; do
-    ${script}
-  done
+  if [[ ! -z ${SCRIPTS} ]]; then
+    for script in ${SCRIPTS}; do
+      ${script}
+    done
+  else
+    # Find requirements
+    REQUIREMENTS=$(find ${CODE_DIR} -name requirements.txt -not -path venv -not -path virtualenv)
+
+    if [[ ! -z ${REQUIREMENTS} ]]; then
+      for requirement in ${REQUIREMENTS}; do
+        pip install -r ${requirement}
+      done
+    fi
+  fi
 fi
 
+# Post Reqs User Scripts
+if [[ -d ${POST_DIR} ]]; then
+  SCRIPTS=$(find ${CODE_DIR}/.crafter/scripts/api/post -type f -name '*.sh')
+
+  if [[ ! -z ${SCRIPTS} ]]; then
+    for script in ${SCRIPTS}; do
+      ${script}
+    done
+  fi
+fi
 
 # Start passenger
 /sbin/my_init
